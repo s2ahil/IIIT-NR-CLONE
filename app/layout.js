c37cbc9e2d84d0a3181e50490f481d4529fd028f
s2ahil <sahilpradhan411@gmail.com>
2024-11-19 17:26:25 +0530

    added meta description

--- app/layout.js
+++ app/layout.js
@@ -3,9 +3,9 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "IIIT NAYA RAIPUR",
-  description: "Website for college students of iiitnr  ",
+export const metadata = { 
+  title: "IIIT NAYA RAIPUR CLONE WEBSITE",
+  description: "clone Website for college students of iiitnr ()  ",
 };
 
 export default function RootLayout({ children }) {
